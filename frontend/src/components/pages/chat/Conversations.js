2695c5673f146f8835e26716286adebcc4b25707
unknown <alar.akilbekov99@gmail.com>
2023-01-17 18:42:56 +0600

    com

--- frontend/src/components/pages/chat/Conversations.js
+++ frontend/src/components/pages/chat/Conversations.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react';
 
+import {useNavigate} from "react-router-dom";
+
 import Workflow from "../../phone/Workflow";
 import Navbar from "../../phone/Navbar";
 import Menu from "../../phone/Menu";
@@ -9,7 +11,6 @@ import Chats from "../../chat/Chats";
 import YummyButton from "../../chat/YummyButton";
 import ChatItem from "../../chat/ChatItem";
 import CreateIcon from "../../../icons/arrow-right.svg"
-import {useNavigate} from "react-router-dom";
 
 export default function Conversations({ conversations, openChat }){
     const navigate = useNavigate();
