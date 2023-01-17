f2b6f074fa8c7c77ec6671ba503cc5f2d4a71962
Ayan <xchjjvg07@gmail.com>
2023-01-17 19:33:08 +0600

    made order application

--- frontend/src/components/chat/ChatItem.js
+++ frontend/src/components/chat/ChatItem.js
@@ -2,6 +2,29 @@ import React, {useEffect, useState} from 'react'
 
 export default function ChatItem({ unread_num, name, last_message, onClick }){
     return (
+        // <div class="card card-order">
+        //     <div class="card-order__info info">
+        //         <div class="info__title">
+        //             Астана - Будапешт
+        //         </div>
+        //         <div class="info__date">
+        //             12 мая - 20 мая
+        //         </div>
+        //         <div class="info__details">
+        //             <span>Командировка</span>
+        //             <span>100 чел.</span>
+        //         </div>
+        //         <div class="info__client">
+        //             <div class="logo-client">
+        //                 <img src="img/logo-client.png" alt="logo-client">
+        //             </div>
+        //             Казмунайгаз
+        //         </div>
+        //     </div>
+        //     <div class="card-order__status status status-new">
+        //         <span>Новый</span>
+        //     </div>
+        // </div>
         <div className={`chat-item ${ unread_num > 0 ? 'unreaded' : '' }`} onClick={onClick}>
             <div className="chat-item__photo">
 
