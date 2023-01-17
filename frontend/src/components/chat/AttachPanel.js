932829858e9ce18a0b86f6b8408a68f1a702c4af
Ayan <xchjjvg07@gmail.com>
2023-01-18 04:38:46 +0600

    attach panel decomposition

--- frontend/src/components/chat/AttachPanel.js
+++ frontend/src/components/chat/AttachPanel.js
@@ -1,13 +1,6 @@
 import React, {useState} from 'react'
 
 import CloseIcon from "../../icons/close.svg";
-<<<<<<< HEAD
-import HouseIcon from "../../icons/house.svg";
-import PersonalCardIcon from "../../icons/personalcard.svg";
-import ArchiveIcon from "../../icons/direct-normal.svg";
-import GalleryIcon from '../../icons/gallery.svg'
-=======
->>>>>>> 296f7332006a3c3307da3312fa1ad9db2deb4d72
 
 export default function AttachPanel({title, children}){
 
@@ -18,33 +11,7 @@ export default function AttachPanel({title, children}){
                     <CloseIcon />
                 </div>
                 <div className="chat-controls-attach__title">
-<<<<<<< HEAD
-                    Выберите паттерн
-                </div>
-                <div className="chat-controls-attach__actions">
-                    <div className="btn btn-main btn-icon btn-attchinchat">
-                        <span>Предложить услугу</span>
-                        <HouseIcon />
-                    </div>
-                    <div className="btn btn-main btn-icon btn-attchinchat">
-                        <span>Получить файлы</span>
-                        <PersonalCardIcon />
-                    </div>
-                    <div className="btn btn-main btn-icon btn-attchinchat">
-                        <span>Отправить файл</span>
-                        <GalleryIcon />
-                    </div>
-                    <div className="btn btn-main btn-icon btn-attchinchat">
-                        <span>Изменить данные</span>
-                        <HouseIcon />
-                    </div>
-                    <div className="btn btn-main btn-icon btn-toarchive">
-                        <span>Отправить в архив</span>
-                        <ArchiveIcon />
-                    </div>
-=======
                     {title} 
->>>>>>> 296f7332006a3c3307da3312fa1ad9db2deb4d72
                 </div>
                 {children}
             </div>
