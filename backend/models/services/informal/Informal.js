a360ffd8b05fec5f69094dc3844b5932f30ed595
unknown <alar.akilbekov99@gmail.com>
2023-01-20 02:24:48 +0600

    comments, Informal

--- backend/models/services/informal/Informal.js
+++ backend/models/services/informal/Informal.js
@@ -1,4 +1,6 @@
 const {Schema, model} = require('mongoose');
+const log = require('../../../log')
+const colors = require("../../../colors");
 
 const InformalSchema = new Schema({
     description: String,
@@ -7,8 +9,11 @@ const InformalSchema = new Schema({
 InformalSchema.plugin(require('mongoose-unique-validator'));
 
 InformalSchema.post('save', function(document, next){
-    if(process.env.REST_LOG === 'needed')
-        console.log('save:', {Informal: document});
+    log(colors.green('saved:'), {Informal: document});
+    next();
+});
+InformalSchema.post('remove', function(document, next){
+    log(colors.green('removed:'), {Informal: document});
     next();
 });
 
