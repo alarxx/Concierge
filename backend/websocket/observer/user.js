8542e6a572b3a7f42e7c4958c04aa233edaa17c6
unknown <alar.akilbekov99@gmail.com>
2023-01-22 23:35:09 +0600

    observer

--- backend/websocket/observer/user.js
+++ backend/websocket/observer/user.js
@@ -2,9 +2,6 @@
  * Идея в том, чтобы уведомлять пользователя о каких-то изменениях в базе данных
  * */
 
-/* passport использует модель User, до инициализации socket.io, поэтому изначально он здесь null */
-const io = require('../../websocket/socket.io').io;
-
 module.exports = function(schema) {
     schema.post('save', function(document, next){
         next();
