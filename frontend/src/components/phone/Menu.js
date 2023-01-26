dd4021b072c45fbfb875d62f38cf8f28d6a82306
unknown <alar.akilbekov99@gmail.com>
2023-01-26 07:48:35 +0600

    menu navigate

--- frontend/src/components/phone/Menu.js
+++ frontend/src/components/phone/Menu.js
@@ -3,29 +3,32 @@ import React from 'react'
 import Category from '../../assets/icons/category.svg'
 import Math from '../../assets/icons/math.svg'
 import Message from '../../assets/icons/messages-3.svg'
+import {useNavigate} from "react-router-dom";
 
 export default function Menu(){
-        return (
-            <div className="menu">
-                <div className="menu__wrapper">
-                    <ul className="menu__links">
-                        <li className="menu__li">
-                            <div className="menu__icon">
-                                <Category width="28" height="28" viewBox="0 0 28 28"/>
-                            </div>
-                        </li>
-                        <li className="menu__li">
-                            <div className="menu__icon">
-                                <Message width="28" height="28" viewBox="0 0 28 28" />
-                            </div>
-                        </li>
-                        <li className="menu__li">
-                            <div className="menu__icon">
-                                <Math width="26" height="26" viewBox="0 0 26 26"/>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+    const navigate = useNavigate();
+
+    return (
+        <div className="menu">
+            <div className="menu__wrapper">
+                <ul className="menu__links">
+                    <li className="menu__li">
+                        <div className="menu__icon" onClick={e => navigate('/profile', {replace: true,})}>
+                            <Category width="28" height="28" viewBox="0 0 28 28"/>
+                        </div>
+                    </li>
+                    <li className="menu__li">
+                        <div className="menu__icon" onClick={e => navigate('/chat', {replace: true,})}>
+                            <Message width="28" height="28" viewBox="0 0 28 28" />
+                        </div>
+                    </li>
+                    <li className="menu__li">
+                        <div className="menu__icon" onClick={e => navigate('/', {replace: true,})}>
+                            <Math width="26" height="26" viewBox="0 0 26 26"/>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        );
+        </div>
+    );
 }
