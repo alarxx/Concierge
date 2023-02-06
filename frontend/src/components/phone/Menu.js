7d7960dd401343283c81b3e8b073735e56dcf604
Alar <alar.akilbekov99@gmail.com>
2023-02-07 05:39:29 +0600

    menu

--- frontend/src/components/phone/Menu.js
+++ frontend/src/components/phone/Menu.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import Category from '../../assets/icons/category.svg'
-import House from '../../assets/icons/house.svg'
+import User from '../../assets/icons/user.svg'
 import Message from '../../assets/icons/messages-3.svg'
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 
 export default function Menu(){
     const navigate = useNavigate();
+
+    const { pathname } = useLocation();
 
     return (
         <div className="menu">
             <div className="menu__wrapper">
                 <ul className="menu__links">
                     <li className="menu__li">
-                        <div className="menu__icon menu__icon-active" onClick={e => navigate('/profile', {replace: true,})}>
+                        <div className={`menu__icon ${pathname==='/profile'?'menu__icon-active':''}`} onClick={e => navigate('/profile', {replace: true,})}>
                             <Category width="28" height="28" viewBox="0 0 28 28"/>
                         </div>
                     </li>
                     <li className="menu__li">
-                        <div className="menu__icon" onClick={e => navigate('/chat', {replace: true,})}>
+                        <div className={`menu__icon ${pathname==='/chat'?'menu__icon-active':''}`} onClick={e => navigate('/chat', {replace: true,})}>
                             <Message width="28" height="28" viewBox="0 0 28 28" />
                         </div>
                     </li>
                     <li className="menu__li">
-                        <div className="menu__icon" onClick={e => navigate('/', {replace: true,})}>
-                            <House width="28" height="28" viewBox="0 0 28 28"/>
+                        <div className={`menu__icon ${pathname==='/'?'menu__icon-active':''}`} onClick={e => navigate('/', {replace: true,})}>
+                            <User width="28" height="28" viewBox="0 0 28 28"/>
                         </div>
                     </li>
                 </ul>
