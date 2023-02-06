a3fea54dd1dc506bc0dec778b48d270435528b6f
Alar <alar.akilbekov99@gmail.com>
2023-02-07 01:09:27 +0600

    admin page middleware

--- frontend/src/pages/new/Admin.js
+++ frontend/src/pages/new/Admin.js
@@ -17,42 +17,45 @@ export default function Admin(){
     }, []);
 
     return (
-        <div className="admin">
-            <Header user={user} pageName="users"></Header>
+        <>{isAuthenticated() && user.role !== 'manager' &&
 
-            <div className="workflow">
-                <div className="container2">
-                    <div className="workflow__wrapper">
-                        <div className="title-admin">
-                            Пользователи
-                        </div>
-                        <div className="tabs">
-                            <ul className="tab dflex aic">
-                                <li className="tab__li tab__li-active"><a href="frontend/src/pages#" data-tab="managers">Менеджеры</a></li>
-                                <li className="tab__li"><a href="frontend/src/pages#" data-tab="b2b">B2B клиенты</a></li>
-                                <li className="tab__li"><a href="frontend/src/pages#" data-tab="b2c">B2С клиенты</a></li>
-                            </ul>
-                        </div>
+            <div className="admin">
+                <Header user={user} pageName="users"></Header>
 
-                        <div className="table" id="managers">
-                            <div className="table__header dflex aic jcsb">
-                                <div className="table__title">
-                                    Главные менеджеры
-                                </div>
-                                <div className="table__btn">
-                                    <div className="btn btn-main">
-                                        Добавить пользователя
+                <div className="workflow">
+                    <div className="container2">
+                        <div className="workflow__wrapper">
+                            <div className="title-admin">
+                                Пользователи
+                            </div>
+                            <div className="tabs">
+                                <ul className="tab dflex aic">
+                                    <li className="tab__li tab__li-active"><a href="frontend/src/pages#" data-tab="managers">Менеджеры</a></li>
+                                    <li className="tab__li"><a href="frontend/src/pages#" data-tab="b2b">B2B клиенты</a></li>
+                                    <li className="tab__li"><a href="frontend/src/pages#" data-tab="b2c">B2С клиенты</a></li>
+                                </ul>
+                            </div>
+
+                            <div className="table" id="managers">
+                                <div className="table__header dflex aic jcsb">
+                                    <div className="table__title">
+                                        Главные менеджеры
+                                    </div>
+                                    <div className="table__btn">
+                                        <div className="btn btn-main">
+                                            Добавить пользователя
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="table__body">
-                                <Table />
+                                <div className="table__body">
+                                    <Table />
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-        </div>
+        }</>
     );
 }
