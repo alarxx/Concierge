2216c9810964ce8ed28c4b02b62fa96f5c1cc3e6
Alar <alar.akilbekov99@gmail.com>
2023-02-07 03:45:23 +0600

    AuthNew multistep

--- frontend/src/pages/new/AuthNew.js
+++ frontend/src/pages/new/AuthNew.js
@@ -1,19 +1,68 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import {useAppContext} from "../../context/AppContext";
 import Header from "./Header";
+import MultistepForm from "../../components/form/MultistepForm";
+import WorkTypes from "../auth/registerForms/WorkTypesForm";
+import UserForm from "../auth/registerForms/UserForm";
+import PasswordsForm from "../auth/registerForms/PasswordsForm";
+import LoginForm from "../auth/loginForms/LoginForm";
 
+const registerForms = [WorkTypes, UserForm, PasswordsForm]
+const loginForms = [LoginForm]
 
+function log(...str){
+    console.log(...str)
+}
 
 export default function AuthNew(){
 
-    const {authHandler} = useAppContext();
-    const {user} = authHandler;
+    const location = useLocation();
+    const navigate = useNavigate();
 
-    const [activeTab, setActiveTab] = useState('signin');
-    const handleTabChange = tab => {
-        setActiveTab(tab);
-    };
+    const {authHandler} = useAppContext();
+    const {user, login, register, userLoading} = authHandler;
+
+    const [data, setData] = useState({})
+    const [type, setType] = useState('login');
+
+    function onSubmit(e){
+        (async ()=>{
+
+            const state = {...location.state};
+
+            if(type === 'login'){
+                const err = await login(data);
+                if(err){
+                    log("Login error", err);
+                    navigate('/authenticate', {replace: true, state});
+                }
+                else{
+                    log("Successful login redirect to", location.state?.redirect ? location.state.redirect : '/');
+
+                    delete state.redirect;
+                    // console.log("navigate to", location.state.redirect, "\nstate", state);
+                    navigate(location.state?.redirect ? location.state.redirect : '/', {replace: true, state});
+                }
+            }
+            else{
+                const err = await register(data);
+                if(err){
+                    log("Registration error", err);
+                    navigate('/authenticate', {replace: true, state});
+                }
+                else{
+                    log("Successful registration redirect to login");
+                    setType('login')
+                    // console.log("navigate to /authenticate", "\nstate", state);
+                    // navigate(location.state?.redirect ? location.state.redirect : '/', {replace: true, state});
+
+                    navigate('/authenticate', {replace: true, state});
+                }
+            }
+
+        })()
+    }
 
     return (
         <div className='admin'>
@@ -29,33 +78,52 @@ export default function AuthNew(){
                                 </div>
                                 <div className="sign__tabs dflex aic">
                                     <span
-                                        className={`sign__tab ${activeTab === 'signin' ? 'sign__tab-active' : ''}`}
-                                        onClick={() => handleTabChange('signin')}
+                                        className={`sign__tab ${type === 'login' ? 'sign__tab-active' : ''}`}
+                                        onClick={() => setType('login')}
                                     >Вход</span>
                                     <span
-                                        className={`sign__tab ${activeTab === 'signup' ? 'sign__tab-active' : ''}`}
-                                        onClick={() => handleTabChange('signup')}
+                                        className={`sign__tab ${type === 'register' ? 'sign__tab-active' : ''}`}
+                                        onClick={() => setType('register')}
                                     >Регистрация</span>
                                 </div>
                             </div>
-                            {activeTab === 'signin' && 
+
+                            {type === 'login' &&
                                 <div className="sign__body">
-                                    <form action="auth">
-                                        <div className="input-form">
-                                            <label htmlFor="people_quantity">Эл. почта *</label>
-                                            <input type="email" name="email" className="input input-choice"
-                                                placeholder="Введите вашу эл. почту" required/>
-                                        </div>
-                                        <div className="input-form">
-                                            <label htmlFor="people_quantity">Пароль</label>
-                                            <input type="password" name="password" className="input input-choice"
-                                                placeholder="Введите пароль" required/>
-                                        </div>
-                                        <button className="btn btn-main" type="submit">Войти</button>
-                                    </form>
+                                    {/*<form action="auth">*/}
+                                    {/*    <div className="input-form">*/}
+                                    {/*        <label htmlFor="people_quantity">Эл. почта *</label>*/}
+                                    {/*        <input type="email" name="email" className="input input-choice"*/}
+                                    {/*            placeholder="Введите вашу эл. почту" required/>*/}
+                                    {/*    </div>*/}
+                                    {/*    <div className="input-form">*/}
+                                    {/*        <label htmlFor="people_quantity">Пароль</label>*/}
+                                    {/*        <input type="password" name="password" className="input input-choice"*/}
+                                    {/*            placeholder="Введите пароль" required/>*/}
+                                    {/*    </div>*/}
+                                    {/*    <button className="btn btn-main" type="submit">Войти</button>*/}
+                                    {/*</form>*/}
+                                    <MultistepForm
+                                        forms={loginForms}
+                                        data={data}
+                                        setData={setData}
+                                        onSubmit={onSubmit}
+                                        submitButtonName={"Войти"}
+                                    />
                                 </div>
                             }
-                            {activeTab === 'signup' && <div>Tab 2 content</div>}
+
+                            {type === 'register' &&
+                                <div className="sign__body">
+                                    <MultistepForm
+                                        forms={registerForms}
+                                        data={data}
+                                        setData={setData}
+                                        onSubmit={onSubmit}
+                                        submitButtonName={"Создать аккаунт"}
+                                    />
+                                </div>
+                            }
                             
                         </div>
                     </div>
