ba3eaa17960cb9c0aa4f4d1865480e4616da3796
Alar <alar.akilbekov99@gmail.com>
2023-02-07 00:26:31 +0600

    add flights services api

--- backend/models/services/flight/Flight.js
+++ backend/models/services/flight/Flight.js
@@ -1,12 +1,19 @@
 const {Schema, model} = require('mongoose');
 
-const Company = require('../../company/Company');
-
 const FlightSchema = new Schema({
     company: {
         type: Schema.Types.ObjectId,
-        ref: 'Company'
+        ref: 'Company',
+        required: true,
+        immutable: true,
     },
+    office: {
+        type: Schema.Types.ObjectId,
+        ref: 'Office',
+        required: true,
+        immutable: true,
+    },
+
     name: {
         type: String,
         required: true,
@@ -17,8 +24,52 @@ const FlightSchema = new Schema({
     placeOfDeparture: {
         type: String,
     },
+    logo: {
+        type: Schema.Types.ObjectId,
+        ref: 'File'
+    }
 });
 
 FlightSchema.plugin(require('mongoose-unique-validator'));
+FlightSchema.plugin(require('../../updatedDate'));
+FlightSchema.plugin(require('../../logPlugin'));
+
+const handlers = require('../../handlers');
+const colors = require("../../../logging/colors");
+
+FlightSchema.statics.publicFiles = function(){
+    // Наверное можно как-то прямо в схему это вписывать {type: Schema.Types.ObjectId, ref: 'File', private: false} типа так
+    return ['logo'];
+}
+
+
+FlightSchema.methods.onCreate = async function({body}){
+    if(body.flight || body.type)
+        return new Error(`You cannot manually enter the 'type' and specific '*service' fields`);
+
+    const Offices = require('../../modelsManager').models.Office;
+
+    const office = new Offices({
+        company: body.company,
+        type: 'flight',
+        flight: this.id
+    });
+
+    this.office = office.id;
+    await office.save();
+}
+
+FlightSchema.methods.deepDelete = async function(){
+    await handlers.deleteModels(this, ['logo', 'office']);
+
+    const Flight_Services = require('../../modelsManager').models.Flight_Service;
+    //Еще надо удалить все Flight/Service, которые принадлежат этому отелю
+    const flights = await Flight_Services.find({flight: this.id});
+    // console.log(colors.red('services[]='), flights);
+    await Promise.all(flights.map(async flight => await flight.deepDelete()));
+
+    await this.delete();
+    return this;
+}
 
 module.exports = model('Flight', FlightSchema);
--- backend/models/services/flight/Flight_Booking.js
+++ backend/models/services/flight/Flight_Booking.js
@@ -3,10 +3,27 @@ const {Schema, model} = require('mongoose');
 // const Booking = require('../BookingAbstract');
 
 const BookingSchema = new Schema({
-    // booking: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Booking',
-    // },
+    booking: {
+        type: Schema.Types.ObjectId,
+        ref: 'Booking',
+        required: true,
+        immutable: true,
+        unique: true,
+    },
+    'flight/service': {
+        type: Schema.Types.ObjectId,
+        ref: 'Flight/Service',
+        required: true,
+        immutable: true,
+    },
+
+    customer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        immutable: true,
+    },
+
     dateOfArrival:{
         type: Date,
     },
@@ -16,5 +33,53 @@ const BookingSchema = new Schema({
 });
 
 BookingSchema.plugin(require('mongoose-unique-validator'));
+BookingSchema.plugin(require('../../updatedDate'));
+BookingSchema.plugin(require('../../logPlugin'));
 
-module.exports = model('Flight_Booking', BookingSchema);
+BookingSchema.methods.onCreate = async function({req, res, body, user}){
+
+    this.customer = user.id;
+
+    // Creating booking
+
+    const booking = new Booking({
+        type: 'hotel/booking',
+        'hotel/booking': this.id
+    });
+    this.booking = booking.id;
+
+    const Flight_Services = require('../../modelsManager').models.Flight_Service;
+
+    if(!body['flight/service'])
+        await this.validate();
+
+    const flight_service = await Flight_Services.findById(body['flight/service']);
+
+    if(!flight_service)
+        return res.status(400).json({error: "Flight/Service not found"});
+
+    booking.service = flight_service.service;
+
+    await booking.save();
+
+    //'flight/service' сетится в модель в controller
+}
+
+const handlers = require("../../handlers");
+
+BookingSchema.methods.deepDelete = async function(){
+    // Должны удалить Bill, File
+    await handlers.deleteModels(this, ['bill', 'file', 'booking']);
+
+    await handlers.deleteArraysOfModels(this, []);
+
+    await this.delete();
+
+    return this;
+}
+
+BookingSchema.virtual('current_price').get(function(){
+    return this.price - this.price * (this.discount / 100)
+});
+
+module.exports = model('Flight/Booking', BookingSchema);
