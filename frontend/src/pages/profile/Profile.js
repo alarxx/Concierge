96a4e5e25e2a7d91f7d1b56cd37735b227a07147
unknown <alar.akilbekov99@gmail.com>
2023-02-07 09:55:02 +0600

    icons

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import Workflow from "../../components/phone/Workflow";
-import Workspace from "../../components/phone/Workspace";
-import Menu from "../../components/phone/Menu";
-import {Link, useNavigate} from "react-router-dom";
-import {useAppContext} from "../../context/AppContext";
-import Icon from "../../assets/icons/add.svg";
 
-function MyLink({title="Page", onClick=f=>f}){
+import AdminIcon from '../../assets/icons/admin.svg'
+import AuthIcon from '../../assets/icons/auth.svg'
+import ChatIcon from '../../assets/icons/chat.svg'
+import DetailsIcon from '../../assets/icons/details.svg'
+import LogOutIcon from '../../assets/icons/log out.svg'
+import MainIcon from '../../assets/icons/main.svg'
+import OrderIcon from '../../assets/icons/order.svg'
+import PartnerIcon from '../../assets/icons/partner.svg'
+import ProfileIcon from '../../assets/strangeIcons/profile.svg'
+
+import Menu from "../../components/phone/Menu";
+import Workspace from "../../components/phone/Workspace";
+import Workflow from "../../components/phone/Workflow";
+import {useNavigate} from "react-router-dom";
+import {useAppContext} from "../../context/AppContext";
+
+function MyLink({
+                    title="Page",
+                    onClick=f=>f,
+                    icon=f=>f}){
     return (
         <div className="push" onClick={onClick}>
-            <div className="push__icon"><Icon /></div>
+            <div className="push__icon">{icon}</div>
             <div className="push__info">
                 <div className="push__title">{title}</div>
             </div>
@@ -39,20 +52,20 @@ export default function Profile(){
                 {userLoading && <p>loading...</p>}
 
                 <div className="profile__pushs">
-                    <MyLink title={"Home page"} onClick={e => navigate('/')}/>
-                    <MyLink title={"Admin page"} onClick={e => navigate('/admin')}/>
+                    <MyLink title={"Home page"} onClick={e => navigate('/')} icon={<MainIcon />}/>
+                    <MyLink title={"Admin page"} onClick={e => navigate('/admin')} icon={<AdminIcon />}/>
 
-                    <MyLink title={"Profile page"} onClick={e => navigate('/profile')}/>
-                    <MyLink title={"Main page"} onClick={e => navigate('/main')}/>
+                    <MyLink title={"Profile page"} onClick={e => navigate('/profile')} icon={<ProfileIcon />}/>
+                    <MyLink title={"Main page"} onClick={e => navigate('/main')} icon={<MainIcon />}/>
 
-                    <MyLink title={"Auth page"} onClick={e => navigate('/authenticate')}/>
-                    <MyLink title={"Log Out page"} onClick={e => navigate('/logout')}/>
+                    <MyLink title={"Auth page"} onClick={e => navigate('/authenticate')} icon={<AuthIcon />}/>
+                    <MyLink title={"Log Out page"} onClick={e => navigate('/logout')} icon={<LogOutIcon />}/>
 
-                    <MyLink title={"Chat page"} onClick={e => navigate('/chat')}/>
-                    <MyLink title={"Order page"} onClick={e => navigate('/order')}/>
+                    <MyLink title={"Chat page"} onClick={e => navigate('/chat')} icon={<ChatIcon />}/>
+                    <MyLink title={"Order page"} onClick={e => navigate('/order')} icon={<OrderIcon />}/>
 
-                    <MyLink title={"Partners page"} onClick={e => navigate('/partners')}/>
-                    <MyLink title={"Details page"} onClick={e => navigate('/details')}/>
+                    <MyLink title={"Partners page"} onClick={e => navigate('/partners')} icon={<PartnerIcon />}/>
+                    <MyLink title={"Details page"} onClick={e => navigate('/details')} icon={<DetailsIcon />}/>
                 </div>
             </Workspace>
 
