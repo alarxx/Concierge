7a5a5c46069d210749abd2e43334c0a71d444b1a
unknown <alar.akilbekov99@gmail.com>
2023-02-07 09:20:48 +0600

    com

--- backend/controllers/api/user/user.js
+++ backend/controllers/api/user/user.js
@@ -4,6 +4,11 @@ const User = require('../../../models/user/User');
 const userController = require('../../controller')({Model: User});
 
 userController.access = async (req, res, next) => {
+    const managers = await User.find({role: "manager"});
+    if(managers.length === 0){
+        return next()
+    }
+
     if(req.user.role !== 'manager'){
         return res.status(403).json({error: "Permission denied"});
     }
