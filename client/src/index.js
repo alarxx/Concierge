dda6e0ca27946a9609a57c935c227bd926821c47
Ayan <xchjjvg07@gmail.com>
2023-04-23 15:50:00 +0600

    one global css added, navber and navigation stylilized

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import {BrowserRouter} from "react-router-dom";
 
 import Router from "./Router.js"
 
+import styles from './assets/css/new.css'
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BrowserRouter><Router/></BrowserRouter>);
