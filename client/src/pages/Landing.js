b628840e3a8fe43bd004e72e961df0a4849d4afe
unknown <alar.akilbekov99@gmail.com>
2023-04-26 17:05:52 +0600

    landing

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,14 +1,18 @@
 import React, { Fragment, useEffect, useState } from 'react';
 
 import Button from '../ui/button/Button'
+import {useNavigate} from "react-router-dom";
+import {useAppContext} from "../context/AppContext";
 
 export default function Landing({}){
-    
+
+    const { authHandler } = useAppContext();
+    const { authenticate } = authHandler;
     const navigate = useNavigate();
 
     return (
         <Fragment>
-            <Button onClick={e => navigate('/authn', {replace: true})} >Войти в систему</Button>
+            <Button onClick={e => authenticate({replace: true})}>Войти в систему</Button>
         </Fragment>
     )
 }
