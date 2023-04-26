9f564df5c6d4c5c906e9afcc3217b17265c6dfbf
Ayan <xchjjvg07@gmail.com>
2023-04-26 17:01:48 +0600

    landing + auth pages

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,43 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
-import Card from '../ui/card/Card';
-import CardHeader from '../ui/card/CardHeader';
-import Logo from '../ui/logo/Logo';
-import CardBody from '../ui/card/CardBody';
-import CardFooter from '../ui/card/CardFooter';
-import TextWithLink from '../ui/text_with_link/TextWithLink'
-import SignIn from '../entities/auth/signin/Signin';
-import Signup from '../entities/auth/signup/Signup';
+import Button from '../ui/button/Button'
 
-
-export default function Main({}){
-
-    const [isSignup, setIsSignup] = useState(false)
-
-    // useEffect(()=> {
-    //     if 
-    // }, [isSignup])
-
-    const handleClick = (isSignup) => {
-        setIsSignup(isSignup);
-    };
+export default function Landing({}){
+    
+    const navigate = useNavigate();
 
     return (
-        <Card>
-            <CardHeader>
-                <Logo />
-            </CardHeader>
-            <CardBody>
-                {isSignup ? <Signup /> : <SignIn /> }
-                
-            </CardBody>
-            <CardFooter>
-                {isSignup 
-                    ? <TextWithLink text="Уже есть аккаунт?" linktext="Авторизация" onClick={() => handleClick(false)} />
-                    : <TextWithLink text="Нет аккаунта?" linktext="Регистрация" onClick={() => handleClick(true)} />
-                }
-                
-            </CardFooter>
-        </Card>
+        <Fragment>
+            <Button onClick={e => navigate('/authn', {replace: true})} >Войти в систему</Button>
+        </Fragment>
     )
 }
--- client/src/pages/auth/main/Authentication.js
+++ client/src/pages/auth/main/Authentication.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {Fragment, useEffect, useMemo, useState} from 'react';
 import {Link, Navigate, NavLink, useLocation, useNavigate, useSearchParams} from "react-router-dom";
 import {useAppContext} from "../../../context/AppContext";
 
@@ -7,6 +7,15 @@ import SignIn from "./SignIn";
 import SendActivationMail from "./SendActivationMail";
 import SendResetPasswordMail from "../password/SendResetPasswordMail";
 
+
+import Card from '../../../ui/card/Card';
+import CardHeader from '../../../ui/card/CardHeader';
+import Logo from '../../../ui/logo/Logo';
+import CardBody from '../../../ui/card/CardBody';
+import CardFooter from '../../../ui/card/CardFooter';
+import TextWithLink from '../../../ui/text_with_link/TextWithLink'
+import SignIn from '../../../entities/auth/signin/Signin';
+import Signup from '../../../entities/auth/signup/Signup';
 /*
 * 1) Не всегда при OAuth2 имеется имя, а в приложении хотелось бы иметь имя всегда.
 * Для этого нужно, если нет имени пользователя, перенаправлять на страницу
@@ -23,38 +32,35 @@ export default function Authentication({ }){
 
     const logger = useMemo(()=>new Logger('Authentication'), []);
 
-    const [type, setType] = useState('signin');
+    const [tabType, setTabType] = useState('signin');
 
     // SignUp/SignIn должны быть в одном компоненте и OAuth тоже, все должно быть в одном Authentication page
-    return (<>
+    return (
+        <Fragment>
 
-        <h1>[Authenticate]</h1>
+            <Card>
+                <CardHeader>
+                    <Logo />
+                </CardHeader>
 
+                <CardBody>
+                    {tabType === 'signup' && <Signup />}
+                    {tabType === 'signin' && <SignIn />}
+                </CardBody>
+                
+                <CardFooter>
+                    {tabType === 'signup' && <TextWithLink text="Уже есть аккаунт?" linktext="Авторизация" onClick={() => setTabType('signin')} />}
+                    {tabType === 'signin' && <TextWithLink text="Нет аккаунта?" linktext="Регистрация" onClick={() => setTabType('signup')} />}
+                </CardFooter>
+            </Card>
 
-        {type === 'signin' &&
-            <SignIn signin={signin}/>
-        }
-        {type === 'sendActivationMail' &&
-            <SendActivationMail sendActivationMail={sendActivationMail} />
-        }
+            <button onClick={e => navigate('/authn/send-reset', {replace: true})}>Forgot Password?</button>
+            <br/><br/>
 
+            <a href={"/auth/azure"}><button>OpenID Connect</button></a>
 
-        {type==='sendActivationMail' &&
-            <button onClick={e => setType('signin')}>already have an account?</button>
-        }
-        {type==='signin' &&
-            <button onClick={e => setType('sendActivationMail')}>don't have an account yet?</button>
-        }
-        <br/><br/>
-
-
-        <button onClick={e => navigate('/authn/send-reset', {replace: true})}>Forgot Password?</button>
-        <br/><br/>
-
-
-        <a href={"/auth/azure"}><button>OpenID Connect</button></a>
-
-    </>);
+        </Fragment>
+    );
 }
 
 /*
