2663449a2c8473842142f8af20b1d78089b5da78
Ayan <xchjjvg07@gmail.com>
2023-04-26 16:43:31 +0600

    comment middleware in index

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,11 +6,11 @@ Router.use('/file', require('./file/file-route'));
 // не все апишки требуют аутентификации
 Router.use('/api', require('./api/api-route'));
 
-Router.use(require('../middlewares/checkAuthenticated'));
+// Router.use(require('../middlewares/checkAuthenticated'));
 
-Router.get('/protected', (req, res)=>{
-    res.json({ secret: "secret", user: req.user });
-});
+// Router.get('/protected', (req, res)=>{
+    // res.json({ secret: "secret", user: req.user });
+// });
 
 
 module.exports = Router;
