207dd2ea4a29c3234d51c822ec44448184c34abf
Ayan <xchjjvg07@gmail.com>
2023-04-30 03:03:14 +0600

    big list

--- client/src/pages/business_client/Orders.js
+++ client/src/pages/business_client/Orders.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {FixedSizeList} from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
 import InfiniteLoader from "react-window-infinite-loader";
@@ -21,6 +21,9 @@ import Button from "../../shared/ui/button/Button";
 import HotelCard from "../../widgets/hotel_card/HotelCard";
 import Configurator from "../../widgets/configurator/Configurator";
 
+function getUrl(skip, limit){
+    return `/api/hotel/pagination?skip=${skip}&limit=${limit}&sort=createdAt`;
+}
 export default function Orders({}){
 
     const [expanded, setExpanded] = React.useState(false);
@@ -30,147 +33,158 @@ export default function Orders({}){
         panel === expanded ? setExpanded(false) : setExpanded(panel)
     };
 
-    const Row = ({index, style}) => {
-        const item = items[index];
-        return (
-            <div className='' style={style}>
-                {item ? item : "Loading..." }
-            </div>
-        )
-    };
 
     // skip - это стартовый индекс
     // limit - это сколько нужно итемов
     // js queryParam чекнуть
-    const getUrl = (skip, limit) => `http://localhost:3000/api/hotel/pagination?skip=${skip}&limit=${limit}`
 
-    const items = {};
-    const requestCache = {};
+    const [items, setItems] = useState({});
+    const [requestCache, setRequestCache] = useState({});
 
-    const isItemLoaded = ({index}) => !!items[index];
+    const isItemLoaded = ({index}) => Boolean(items[index]);
 
-    const loadMoreItems = async (visibleStartIndex, visibleStopIndex) => {
-        const key = [visibleStartIndex, visibleStopIndex].join(":");
+    const loadMoreItems = async (startIndex, stopIndex) => {
+        const key = [startIndex, stopIndex].join(":");
+
         if (requestCache[key]) {
             return;
         }
-        const length = visibleStopIndex - visibleStartIndex;
+
+        const length = stopIndex - startIndex;
+
         const visibleRange = [...Array(length).keys()].map(
-            x=> x+visibleStartIndex
+            x => x + startIndex
         );
-        const itemsRetrieved = visibleRange.every(index => !!items[index])
+
+        const itemsRetrieved = visibleRange.every(index => Boolean(items[index]))
 
         if (itemsRetrieved) {
-            requestCache[key] = key;
+            // requestCache[key] = key;
+            setRequestCache({...requestCache, key});
             return;
         }
-        console.log(key)
 
-        return await fetch(getUrl(visibleStartIndex, length))
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                data.records.forEach((hotel, index) => {
-                    items[index+visibleStartIndex] = hotel;
-                })
+        console.log("retrieve -", key);
+
+        return await fetch(getUrl(startIndex, length))
+            .then(async response => {
+                const json = await response.json();
+
+                console.log({json}); // json = [{}, {}]
+
+                const add = {};
+                json.forEach((hotel, index) => {
+                    add[startIndex + index] = hotel;
+                });
+                setItems({...items, ...add});
             })
-            .catch(error => console.error("Error:", error))
+            .catch(error => console.error(error))
     }
 
-    return (
-        <Fragment>
-            <NavbarPanel title={'Заказы'} />
-            <Box>
-                <div className="section section-profile">
-                    orders
+    const Row = ({index, style}) => {
+        const item = items[index];
+        return (<>
+            {/*{console.log(style)}*/}
+            {item
+                ? <HotelCard title={item.name} price={'от 50,000 KZT '} addInfo={'2 взрослых, 2 ночи'} />
+                : "Loading..." }
+        </>)
+    };
 
-                    <HotelCard title={'Hilton'} price={'от 50,000 KZT '} addInfo={'2 взрослых, 2 ночи'} />
 
-                    <AutoSizer>
-                        {({height, width}) => (
-                            <InfiniteLoader
-                                isItemLoaded={isItemLoaded}
-                                loadMoreItems={loadMoreItems}
-                                itemCount={1000}
-                            >
-                                {({onItemsRendered, ref}) => (
-                                    <FixedSizeList
-                                        className={'List'}
-                                        width={width}
-                                        height={height}
-                                        itemCount={1000}
-                                        itemSize={35}
-                                        ref={ref}
-                                        onItemsRendered={onItemsRendered}
-                                    >
-                                        {Row}
-                                    </FixedSizeList>
-                                )}
-                            </InfiniteLoader>
-                        )}
-                    </AutoSizer>
+    return (<>
+        <NavbarPanel title={'Заказы'} />
+        <Box>
+            <div className="section section-profile">
+                orders
 
-                    {/*<Accordion expanded={expanded === 'panel1'}>*/}
-                    {/*    <AccordionSummary onClick={() => handleChange('panel1')} >*/}
-                    {/*        <Card variant='info'>*/}
-                    {/*            <CardBody>*/}
-                    {/*                <GroupFlex align='aic' justify='jcsb'>*/}
-                    {/*                    AccordionSummary11111*/}
-                    {/*                    <TriangleIcon/>*/}
-                    {/*                </GroupFlex>*/}
-                    {/*            </CardBody>*/}
-                    {/*        </Card>*/}
-                    {/*    </AccordionSummary>*/}
-                    {/*    <AccordionDetails>*/}
-                    {/*        <Card variant='info'>*/}
-                    {/*            <CardBody>*/}
-                    {/*                <EmployeeInfo />*/}
-                    {/*            </CardBody>*/}
-                    {/*        </Card>*/}
-                    {/*    </AccordionDetails>*/}
-                    {/*</Accordion>*/}
-                    {/*<Accordion expanded={expanded === 'panel12'}>*/}
-                    {/*    <AccordionSummary onClick={() => handleChange('panel12')} >*/}
-                    {/*        <Card variant='info'>*/}
-                    {/*            <CardBody>*/}
-                    {/*                <GroupFlex align='aic' justify='jcsb'>*/}
-                    {/*                    Командировка #000421*/}
-                    {/*                    <TriangleIcon/>*/}
-                    {/*                </GroupFlex>*/}
-                    {/*            </CardBody>*/}
-                    {/*        </Card>*/}
-                    {/*    </AccordionSummary>*/}
-                    {/*    <AccordionDetails>*/}
-                    {/*        <Configurator />*/}
-                    {/*    </AccordionDetails>*/}
-                    {/*</Accordion>*/}
 
-                    {/*<Accordion expanded={expanded === 'panel13'} >*/}
-                    {/*    <AccordionSummary onClick={() => handleChange('panel13')} >*/}
-                    {/*        <Card variant='info'>*/}
-                    {/*            <CardBody>*/}
-                    {/*                <GroupFlex align='aic' justify='jcsb'>*/}
-                    {/*                    AccordionSummary11111*/}
-                    {/*                    <TriangleIcon/>*/}
-                    {/*                </GroupFlex>*/}
-                    {/*            </CardBody>*/}
-                    {/*        </Card>*/}
-                    {/*    </AccordionSummary>*/}
-                    {/*    <AccordionDetails>*/}
-                    {/*        <Card variant='info'>*/}
-                    {/*            <CardBody>*/}
-                    {/*                AccordionDetails1111*/}
-                    {/*            </CardBody>*/}
-                    {/*        </Card>*/}
-                    {/*    </AccordionDetails>*/}
-                    {/*</Accordion>*/}
+                {/*<AutoSizer>*/}
+                {/*    {({height, width}) => (*/}
+                        <InfiniteLoader
+                            isItemLoaded={isItemLoaded}
+                            loadMoreItems={loadMoreItems}
+                            itemCount={1000}
+                            minimumBatchSize={3}
+                            threshold={3}
+                        >
+                            {({onItemsRendered, ref}) => (
+                                <FixedSizeList
+                                    className={'List'}
+                                    width={1000}
+                                    height={770}
+                                    itemCount={1000}
+                                    itemSize={300}
+                                    ref={ref}
+                                    onItemsRendered={onItemsRendered}
+                                >
+                                    {Row}
+                                </FixedSizeList>
+                            )}
+                        </InfiniteLoader>
+                    {/*)}*/}
+                {/*</AutoSizer>*/}
 
-                </div>
-            </Box>
-            <BottomControl>
-                <Button variant={'control'} size={'big'} onClick={f=>f}>Заказать услугу</Button>
-            </BottomControl>
-            <NavigationPanel />
-        </Fragment>
-    )
+                {/*<Accordion expanded={expanded === 'panel1'}>*/}
+                {/*    <AccordionSummary onClick={() => handleChange('panel1')} >*/}
+                {/*        <Card variant='info'>*/}
+                {/*            <CardBody>*/}
+                {/*                <GroupFlex align='aic' justify='jcsb'>*/}
+                {/*                    AccordionSummary11111*/}
+                {/*                    <TriangleIcon/>*/}
+                {/*                </GroupFlex>*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </AccordionSummary>*/}
+                {/*    <AccordionDetails>*/}
+                {/*        <Card variant='info'>*/}
+                {/*            <CardBody>*/}
+                {/*                <EmployeeInfo />*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </AccordionDetails>*/}
+                {/*</Accordion>*/}
+                {/*<Accordion expanded={expanded === 'panel12'}>*/}
+                {/*    <AccordionSummary onClick={() => handleChange('panel12')} >*/}
+                {/*        <Card variant='info'>*/}
+                {/*            <CardBody>*/}
+                {/*                <GroupFlex align='aic' justify='jcsb'>*/}
+                {/*                    Командировка #000421*/}
+                {/*                    <TriangleIcon/>*/}
+                {/*                </GroupFlex>*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </AccordionSummary>*/}
+                {/*    <AccordionDetails>*/}
+                {/*        <Configurator />*/}
+                {/*    </AccordionDetails>*/}
+                {/*</Accordion>*/}
+
+                {/*<Accordion expanded={expanded === 'panel13'} >*/}
+                {/*    <AccordionSummary onClick={() => handleChange('panel13')} >*/}
+                {/*        <Card variant='info'>*/}
+                {/*            <CardBody>*/}
+                {/*                <GroupFlex align='aic' justify='jcsb'>*/}
+                {/*                    AccordionSummary11111*/}
+                {/*                    <TriangleIcon/>*/}
+                {/*                </GroupFlex>*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </AccordionSummary>*/}
+                {/*    <AccordionDetails>*/}
+                {/*        <Card variant='info'>*/}
+                {/*            <CardBody>*/}
+                {/*                AccordionDetails1111*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </AccordionDetails>*/}
+                {/*</Accordion>*/}
+
+            </div>
+        </Box>
+        <BottomControl>
+            <Button variant={'control'} size={'big'} onClick={f=>f}>Заказать услугу</Button>
+        </BottomControl>
+        <NavigationPanel />
+    </>)
 }
