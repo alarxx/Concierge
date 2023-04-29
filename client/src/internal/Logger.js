7440621c0a8ab9a9c40c9bf2e2ad845d5a6ae12f
unknown <alar.akilbekov99@gmail.com>
2023-04-30 05:45:40 +0600

    logger auto .js adding

--- client/src/internal/Logger.js
+++ client/src/internal/Logger.js
@@ -1,15 +1,15 @@
 export default class Logger {
     constructor(moduleName) {
-        this.moduleName = moduleName;
+        this.moduleName = moduleName.endsWith('.js') ? moduleName : `${moduleName}.js`;
     }
     log(...str){
         if(process.env.NODE_ENV === 'development') {
-            console.log(`${this.moduleName}.js:`,...str);
+            console.log(`${this.moduleName}:`,...str);
         }
     }
     error(...str){
         if(process.env.NODE_ENV === 'development') {
-            console.error(`${this.moduleName}.js:`,...str);
+            console.error(`${this.moduleName}:`,...str);
         }
     }
 
