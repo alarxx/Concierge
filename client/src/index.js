fe0cec0df51e39859cbe12682c548018c96c36f0
unknown <alar.akilbekov99@gmail.com>
2023-05-10 01:15:14 +0600

    css #root in index.html

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 
 import Router from "./Router.js"
 
-import './assets/css/global.css'
+import './assets/css/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
