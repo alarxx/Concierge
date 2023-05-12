8f5aab1ccd276d3434478e3c3cd9968b289c9516
unknown <alar.akilbekov99@gmail.com>
2023-05-13 03:44:53 +0600

    logo

--- client/src/shared/ui/logo/Logo.js
+++ client/src/shared/ui/logo/Logo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import styles from './logo.module.css' ;
-const logo = require('../../../assets/images/logo.png');
+
 export default function Logo(){
 
     return (
         <div className={styles.logo}>
-            <img src={String(logo)}/>
+            <img src={'/images/logo.png'}/>
         </div>
     );
 }
