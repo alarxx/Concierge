1fb3e33f20c19b867af33ac34749d955934bef24
unknown <alar.akilbekov99@gmail.com>
2023-05-13 20:23:36 +0600

    delete useless hook

--- client/src/context/hooks/loading/useLoading.js
+++
@@ -1,9 +0,0 @@
-import React, {useState} from 'react';
-
-export default function useLoading(){
-    const [loading, setLoading] = useState(false);
-    return ({
-        loading,
-        setLoading
-    });
-}
