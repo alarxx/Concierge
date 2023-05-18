6f923a1f657cc52bcf94d7f7f9f67e83caccba1b
unknown <alar.akilbekov99@gmail.com>
2023-05-18 08:35:33 +0600

    error

--- client/src/features/auth/no_name/NoNameForm.js
+++ client/src/features/auth/no_name/NoNameForm.js
@@ -5,11 +5,12 @@ import {useNavigate} from "react-router-dom";
 import {useAppContext} from "../../../context/AppContext";
 import Button from "../../../shared/ui/button/Button";
 import Input from "../../../shared/ui/input/Input";
+import fetchJSON from "../../../internal/fetchJSON";
 
 export default function NoNameForm({ }){
 
     const {authHandler} = useAppContext();
-    const {assignName, userError} = authHandler;
+    const {assignName} = authHandler;
 
     const [name1, setName1] = useState('');
     const [name2, setName2] = useState('');
@@ -18,39 +19,52 @@ export default function NoNameForm({ }){
     const logger = useMemo(()=>new Logger('NoName'),[])
 
     const [success, setSuccess] = useState(null);
-    const [error, setError] = useState('asdf');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
 
-    async function onSetName(){
+    async function onSetName(e){
+        e.preventDefault();
+
         setSuccess(null);
+        setError(null);
+        setLoading(true);
 
-        try{
-            const json = await assignName({ name: `${name1} ${name2}`, phone })
-            logger.log("USER PHONE",json)
-            if(json.status === 200){
-                setSuccess(json);
-                setError(null);
-            }
-            else {
-                logger.log("USER ERROR",json)
-                logger.log("USER ERROR MESSAGE",json.message)
-                setError(json.message);
-            }
-        }catch(e){
-            setError(e.message);
+        const response = await fetch('/auth/name', {
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            method: "PATCH",
+            body: JSON.stringify({ name:`${name1} ${name2}`, phone })
+        });
+
+        const json = await response.json();
+
+        if(response.status === 200){
+            setSuccess(json);
+            setError(null);
         }
+        else {
+            logger.log("USER ERROR",json)
+            logger.log("USER ERROR MESSAGE",json.message)
+            setError(json);
+        }
+
+        setLoading(false)
 
     }
 
     useEffect(()=> {
-        logger.log("USER ERROR EFFECT",userError)
-    }, [userError])
+        logger.log("USER ERROR EFFECT", error);
+    }, [error])
 
     return (<>
-        <form>
+        <form onSubmit={onSetName}>
             <h1>Не хватает обязательных полей</h1>
 
             {success && <p>{success.message}</p>}
-            {userError && <p>{userError.message}</p>}
+            {error && <p>{JSON.stringify(error.errors)}</p>}
+            {loading && <p>loading...</p>}
 
             <label>Имя</label>
             <Input value={name1} onChange={e => setName1(e.target.value)} required/>
@@ -59,9 +73,9 @@ export default function NoNameForm({ }){
             <Input value={name2} onChange={e => setName2(e.target.value)} required/>
 
             <label>Номер телефона</label>
-            <Input value={phone} onChange={e => setPhone(e.target.value)} required/>
+            <Input type="number" value={phone} onChange={e => setPhone(e.target.value)} required/>
 
-            <Button type={'submit'} onClick={onSetName}>Сохранить</Button>
+            <Button type={'submit'} >Сохранить</Button>
         </form>
     </>);
 }
