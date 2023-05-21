10a9183f28f32469def9bbb011897f71e6fe98a5
Ayan <xchjjvg07@gmail.com>
2023-05-22 01:27:45 +0600

    chat input autofocus, text with link size bigger

--- client/src/features/chat/chat_input_form/ChatInputForm.js
+++ client/src/features/chat/chat_input_form/ChatInputForm.js
@@ -51,6 +51,7 @@ export default function ChatInputForm({
                             onTextSend()
                         }
                     }}
+                    autoFocus={true}
                 />
             </Block>
 
