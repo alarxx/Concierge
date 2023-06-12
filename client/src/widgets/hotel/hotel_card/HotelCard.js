0aeacbb35e2ab580acb3cf1d483b4a058ea9736a
Ayan <xchjjvg07@gmail.com>
2023-06-12 19:35:18 +0600

    smth photos added

--- client/src/widgets/hotel/hotel_card/HotelCard.js
+++ client/src/widgets/hotel/hotel_card/HotelCard.js
@@ -14,6 +14,8 @@ import SliderNew from "../../../shared/slider_new/SliderNew";
 
 export default function HotelCard({title='', stars=0, price='', addInfo='', images=[], onClick=f=>f}) {
 
+    const photos = ['https://www.discoverlosangeles.com/sites/default/files/business/hilton-pasadena/h_2000-crm-la-1-hilton-pasadena_port-cochure-e454ef6c5056a36_e454f09a-5056-a36f-233cbb308efaf59d.jpg','https://avatars.mds.yandex.net/get-altay/998237/2a00000161e27a91e164a409a740f64aced7/XXL','https://venturesafrica.com/wp-content/uploads/2018/10/153050202-1-1024x661.jpg','https://www.theartofbusinesstravel.com/wp-content/uploads/2017/04/92275707.jpg','https://salon.ru/storage/thumbs/gallery/179/178992/5000_5000_s763.jpg','https://images.acase.ru/acase_images/1310763/204783592_P.jpg','https://archinect.imgix.net/uploads/9d/9ddc50df6e0ced48dc1b06785a946531.jpg?fit=crop&amp;auto=compress%2Cformat&amp;w=1200','https://images.acase.ru/acase_images/1310763/204733943_P.jpg'];
+
     return(<>
         <CardService onClick={onClick}>
             <CardServiceHeader>
@@ -21,7 +23,7 @@ export default function HotelCard({title='', stars=0, price='', addInfo='', imag
                 {title}
             </CardServiceHeader>
             <CardServiceBody>
-                <SliderNew photos={['/file/646b038f1b08015b5e5ba2b6']} />
+                <SliderNew photos={photos} />
                 {/*<Gallery images={images.map(id=>`/file/${id}`)} height={160} />*/}
             </CardServiceBody>
             <CardServiceFooter>
